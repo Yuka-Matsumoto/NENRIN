54296874ac8749f251a67124b9fd2132ed0a0059
松本友花 <yuka@matsumotoyukanonotobukkukonpyuta.local>
2024-10-18 22:04:59 +0900

    ✅ Connected API to the database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,24 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
-    'DATABASE_URL', 'postgresql://lastProject_user:lastProject@db/lastProject')
+# SQLAlchemy と Migrate のインスタンス作成
+db = SQLAlchemy()
+migrate = Migrate()
 
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
 
-# ここで models ディレクトリ内のすべてのモデルをインポート
-from app.models import *
+    # 環境変数から接続情報を取得
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://lastProject_user:lastProject@db/lastProject')
+
+    # SQLAlchemyとFlask-Migrateをアプリに初期化
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # ルートやBlueprintを登録
+    from app.routes.search import search_bp
+    app.register_blueprint(search_bp)
+
+    return app
