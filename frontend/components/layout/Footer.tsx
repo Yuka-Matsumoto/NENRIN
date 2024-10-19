96b78df9433cf2a23f2b0f0a2e1ec46e644f6960
キノ0510 <kinont0510@gmail.com>
2024-10-19 20:49:28 +0900

    🚧footer text-align-center

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <p>© 2024 N E N R I N. All rights reserved.</p>
+        <div className="container mx-auto text-center">
+            <p>© 2024 N E N R I N. All rights reserved.</p>
+        </div>
     )
 }
 
