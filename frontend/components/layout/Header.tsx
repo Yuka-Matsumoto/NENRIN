403d36c238481d9eb8c39305a8608b8ad89678f4
キノ0510 <kinont0510@gmail.com>
2024-10-19 21:14:05 +0900

    ✨login button

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div className="text-brown-700 text-3xl font-bold p-4">
-            <h1>N E N R I N</h1>
+        <div className="flex justify-between items-center">
+            <h1 className="text-brown-700 text-3xl font-bold p-4">N E N R I N</h1>
+            <p className="text-brown-700 ml-auto pr-4">L O G I N(会員の方)</p>
         </div>
     )
 }
