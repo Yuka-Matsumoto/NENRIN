20adc5f2c27ce29aab81dd8b06c4572a6864b516
キノ0510 <kinont0510@gmail.com>
2024-10-19 17:55:49 +0900

    🚧add UnionUseButton to top

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@
 
 // import { useEffect, useState } from 'react';
 import SeniorUseButton from '../components/elements/seniorUseButton';  // UserButtonコンポーネントをインポート
-
+import UnionUseButton from '../components/elements/unionUseButton';
 
 export default function HomePage() {
   // ボタンがクリックされたときに実行する関数
@@ -17,6 +17,7 @@ export default function HomePage() {
         <p>シニアの方はこちら</p>
         <SeniorUseButton onClick={handleClick} />
         <p>団体、自治体の方はこちら</p>
+        <UnionUseButton onClick={handleClick} />
       </main>
     </div>
   );
