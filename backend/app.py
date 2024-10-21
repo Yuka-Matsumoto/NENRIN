ce17e36d97f252c16eaaeb1fcba63b9874472e3a
yuna <qp.m.www@gmail.com>
2024-10-21 14:53:17 +0900

    :art: add Bliueprint of job-posting api

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from app.routes.users import users_bp
+from app.routes.jobs import jobs_bp
 
 app = Flask(__name__)
 
@@ -15,6 +16,8 @@ migrate = Migrate(app, db)  # Flask-MigrateとSQLAlchemyを関連付け
 
 # Blueprintをアプリに登録
 app.register_blueprint(users_bp, url_prefix='/api')  #　/api/resister-seniorエンドポイントが有効になる
+app.register_blueprint(jobs_bp, url_prefix='/api')  # /api/job-posting
+
 
 # ログ出力で確認
 print("Before db.init_app")
