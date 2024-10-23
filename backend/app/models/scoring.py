7f4355cb551f6de7584dd80862eb90f12da099f9
松本友花 <yuka@matsumotoyukanonotobukkukonpyuta.local>
2024-10-23 14:32:47 +0900

    ✨feat: Add data to the scoring table

--- backend/app/models/scoring.py
+++ backend/app/models/scoring.py
@@ -20,63 +20,73 @@ def seed_scoring():
         # サンプルデータを追加
         scoring_data = [
             Scoring(
-                application_id="f305c46a-b6a4-4b11-965e-002bb50fdb2e",  # 応募ID
+                application_id="0cd4166a-572b-47b6-9d85-a5af12812125",  # 高橋 健一
                 score=85,
                 criteria_met={
-                    "経験業種": "IT",
+                    "業種": "IT",
+                    "職種": "ソフトウェアエンジニア",
                     "経験年数": 30,
-                    "現在仕事": True,
+                    "現在仕事": False,
+                    "現在勉強中": False,
                     "趣味": True,
-                    "一人暮らし": False,
-                    "外出頻度": "週一日以上"
+                    "一人暮らし": True,
+                    "週一日以上外出": True
                 }
             ),
             Scoring(
-                application_id="391f4b51-fb82-4532-ba69-931b623c80af",  # 応募ID
+                application_id="a954877b-1d11-44bd-8f22-d8678a1825ad",  # 佐藤 花子
                 score=90,
                 criteria_met={
-                    "経験業種": "教育",
+                    "業種": "教育",
+                    "職種": "教員",
                     "経験年数": 20,
                     "現在仕事": True,
-                    "趣味": False,
-                    "一人暮らし": True,
-                    "外出頻度": "週一日以上"
-                }
-            ),
-            Scoring(
-                application_id="446e2ee9-8025-4226-821f-af89d585fef5",  # 応募ID
-                score=75,
-                criteria_met={
-                    "経験業種": "建設",
-                    "経験年数": 40,
-                    "現在仕事": False,
-                    "趣味": True,
-                    "一人暮らし": True,
-                    "外出頻度": "週一日以上"
-                }
-            ),
-            Scoring(
-                application_id="ee7143e0-de33-48b4-b4bf-2385d4a2965a",  # 応募ID
-                score=80,
-                criteria_met={
-                    "経験業種": "ヘルスケア",
-                    "経験年数": 25,
-                    "現在仕事": True,
+                    "現在勉強中": False,
                     "趣味": True,
                     "一人暮らし": False,
-                    "外出頻度": "週一日以上"
+                    "週一日以上外出": True
                 }
             ),
             Scoring(
-                application_id="e0aea1f7-9c6f-4bde-b643-d7ac4f1d4414",  # 応募ID
-                score=70,
+                application_id="16b00ea2-3a78-4729-a570-d6c976363226",  # 鈴木 大輔
+                score=75,
                 criteria_met={
-                    "経験業種": "製造",
-                    "経験年数": 35,
+                    "業種": "建設",
+                    "職種": "建築士",
+                    "経験年数": 40,
                     "現在仕事": True,
+                    "現在勉強中": False,
                     "趣味": False,
                     "一人暮らし": True,
-                    "外出頻度": "週一日以上"
+                    "週一日以上外出": True
+                }
+            ),
+            Scoring(
+                application_id="b83b8907-f62d-43b4-a04f-d3ac3a306b75",  # 中村 美紀
+                score=80,
+                criteria_met={
+                    "業種": "ヘルスケア",
+                    "職種": "看護師",
+                    "経験年数": 25,
+                    "現在仕事": False,
+                    "現在勉強中": True,
+                    "趣味": True,
+                    "一人暮らし": False,
+                    "週一日以上外出": True
+                }
+            ),
+            Scoring(
+                application_id="aca977ee-8a02-4006-b2eb-45041cffdfc0",  # 小林 健二
+                score=70,
+                criteria_met={
+                    "業種": "製造",
+                    "職種": "生産管理者",
+                    "経験年数": 35,
+                    "現在仕事": True,
+                    "現在勉強中": False,
+                    "趣味": False,
+                    "一人暮らし": True,
+                    "週一日以上外出": False
                 }
             )
         ]
