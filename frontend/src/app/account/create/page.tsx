2db2de2d2fac45a701a3fdbb84fb4350bbc78cb6
キノ0510 <kinont0510@gmail.com>
2024-10-25 01:39:28 +0900

    🎨NENRIN router

--- frontend/src/app/account/create/page.tsx
+++ frontend/src/app/account/create/page.tsx
@@ -2,7 +2,7 @@ import SignupForm from '../../../../components/Auth/SignupForm';
 
 const SignupPage = () => {
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-[#ecf7f2] px-4">
+        <div className="flex flex-col items-center justify-center bg-[#ecf7f2] px-4">
             <h1 className="text-3xl font-bold mt-16 mb-8 text-teal-700">新規登録の方はこちらから</h1>
             <SignupForm />
         </div>
